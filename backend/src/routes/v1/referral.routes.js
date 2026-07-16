d05ceb47b54d3d602ad8f91ef71c98380bd495a1
Debmalya Panda <debmalyap229@gmail.com>
2026-07-16 13:32:05 +0530

    fix: alias /my-code route to getReferralQR to match Swagger spec definition

--- backend/src/routes/v1/referral.routes.js
+++ backend/src/routes/v1/referral.routes.js
@@ -6,5 +6,6 @@ const authMiddleware = require('../../middleware/auth.middleware');
 router.get('/', authMiddleware, referralController.getReferrals);
 router.get('/stats', authMiddleware, referralController.getStats);
 router.get('/qr', authMiddleware, referralController.getReferralQR);
+router.get('/my-code', authMiddleware, referralController.getReferralQR);
 
 module.exports = router;
