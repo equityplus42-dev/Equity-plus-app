4f76be502b0b2b14d61bdc0d2e9aa4d66e2a7491
Debmalya Panda <debmalyap229@gmail.com>
2026-07-30 10:34:13 +0530

    fix: update admin email to admin@equityplus.com

--- backend/create_admin.js
+++ backend/create_admin.js
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 
 async function main() {
   try {
-    const adminEmail = 'admin@equilty.com';
+    const adminEmail = 'admin@equityplus.com';
     const rawPassword = 'AdminPassword123!';
     
     // Check if an admin exists
