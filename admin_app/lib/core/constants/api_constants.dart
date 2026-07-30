f81f986f4bc3d5892deb5ea04f059d30209f7abd
Debmalya Panda <debmalyap229@gmail.com>
2026-07-30 10:53:54 +0530

    config: enable useLocalBackend for local testing

--- admin_app/lib/core/constants/api_constants.dart
+++ admin_app/lib/core/constants/api_constants.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
   // Set to true to connect to your local backend.
   // Set to false (default) to connect to the live Vercel backend.
-  static const bool useLocalBackend = false;
+  static const bool useLocalBackend = true;
 
   // Emulator default: 10.0.2.2 routes to host machine's localhost.
   // For physical device run: flutter run --dart-define=LOCAL_API_URL=http://192.168.31.64:5000/api/v1
